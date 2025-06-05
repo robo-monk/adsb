12c6f681eadb86e6592fe300ef02f5c3ef7bd284
robo-monk <rrobomonk@gmail.com>
2025-06-05 23:17:53 +0200

    Update App.svelte to use new data source for aircraft signals and restore WebSocket functionality. Enhanced aircraft count display logic and added visual indicators for aircraft status. Implemented cleanup for WebSocket connection on component destruction.

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
+  import { onDestroy } from "svelte";
   import {
     processIncomingAircraftSignal,
     aircrafts,
     receiverBitrates,
   } from "./lib.svelte";
-  import bigass from "./bigadsb.jsonl?raw";
+  // import bigass from "./bigadsb.jsonl?raw";
+  import bigass from "./out-ads-b.jsonl?raw";
 
   let cursor = 0;
   const entries = bigass
@@ -31,18 +33,18 @@
     }
   }
 
-  next();
+  // next();
 
   // connect to ws at ws://192.87.172.71:1338
-  // const ws = new WebSocket("ws://192.87.172.71:1338");
+  const ws = new WebSocket("ws://192.87.172.71:1338");
 
-  // ws.onmessage = (event) => {
-  //   processIncomingAircraftSignal(event.data)
-  // };
+  ws.onmessage = (event) => {
+    processIncomingAircraftSignal(event.data)
+  };
 
-  // onDestroy(() => {
-  //   ws.close();
-  // });
+  onDestroy(() => {
+    ws.close();
+  });
 </script>
 
 <main class="relative">
@@ -56,7 +58,8 @@
     {/if}
     <div class="flex gap-4 absolute top-0 right-0 p-4 flex-col">
       <div class="text-white text-xs">
-        Aircraft count: {aircrafts.size}
+        Aircraft count: {Object.keys(aircrafts).length}
+        In range: {Object.values(aircrafts).filter((aircraft) => !aircraft.hidden).length}
       </div>
       <div class="text-red-500 font-mono text-xs">
         zi-5067: {receiverBitrates.get("zi-5067")?.toFixed(2) || 0} signals/sec
@@ -75,7 +78,9 @@
         <div
           class="absolute flex flex-col items-center justify-center transition-all duration-200"
           style="left: {(aircraft.longitude * 10 - 40) *
-            2}%; top: {(aircraft.latitude - 50) * 20}%;"
+            2}%; top: {(aircraft.latitude - 50) * 20}%;
+            opacity: {rssiPercentage};
+            "
         >
           <span class="text-white text-xs">{aircraft.address}</span>
           <div
@@ -88,6 +93,7 @@
             <div
               class="w-[1px] h-2 bg-white absolute left-1/2 -translate-x-1/2 -translate-y-[100%]"
             ></div>
+            <div class="text-white text-xs -rotate-90">✈</div>
           </div>
           <!-- <span class="text-white text-xs">{aircraft.address}</span> -->
           <span class="text-white text-xs">{aircraft.altitude}m</span>
